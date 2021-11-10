483ffc879c43af5d7863a9cd063979613536477e
Stayermax <vitalkrat@gmail.com>
2021-11-10 18:32:28 +0300

    All lessons with records
    example finished

--- main.py
+++ main.py
@@ -11,95 +11,214 @@ pd.set_option('display.max_columns', None)
 pd.set_option("max_colwidth", None)
 
 
-# unnecessary
+# Libraries for my example
 import os
 from pprint import pprint
+from datetime import datetime, timedelta
+import time
+import math
+import json
 
-def func_df(function):
+def data_load(func, entity_name, params = None, load_new_data = True):
     """
-    Generic function for DataFrame creation
-    """
-    l = function()
-    df = pd.DataFrame(l)
-    print(df.columns)
-    return df
+    Function loads data entities and transfers them into dataframe
 
-def resp_df(dict_list):
-    """
-    Generic function for DataFrame creation
-    """
-    df = pd.DataFrame(dict_list)
-    print(df.columns)
-    return df
+    :param func: function that requests data from server and returns data in format:
+        {
+            "entity_name": [ {...}, {...}] , # list of dictionaries
+            "stats": {
+                "totalItems": 5
+            }
+        },
+        or in format:
+        [ { ... } ] # list of dictionaries
+    :param entity_name: name of the data returned
+        First format entity_name examples: "users", "lessonRecords", "lessons", "joins"
+        Second format entity_name examples: "filials", "rooms", "managers"
+    :param params: QUERY PARAMETERS ( options can be seen on https://api.moyklass.com/ for each entity_name)
+    :return: dataframe with data
 
-def make_or_read(path, func):
-    if (os.path.exists(path)):
-        df = pd.read_csv(path)
+    """
+    data_path = f"saved_data/{entity_name}_df.csv"
+    if (os.path.exists(data_path) and load_new_data == False):
+        df = pd.read_csv(data_path)
     else:
-        df = make_df(func)
-        df.to_csv(path, index=False)
+        if (params == None):
+            params = []
+        first_response = func(params)
+        if(type(first_response) == dict):
+            items_num = first_response['stats']['totalItems']
+            print(f"Number of {entity_name} with requested params: {items_num}")
+            pages_num = math.ceil(items_num / 100)
+            start = datetime.now()
+            full_list = []
+            for i in range(pages_num):
+                full_list += func(params + [['offset', f'{100 * i}']])[entity_name]
+            df = pd.DataFrame(full_list)
+            print(f"data loaded in {datetime.now() - start} ")
+        else:
+            print(entity_name)
+            df = pd.DataFrame(first_response)
+        df.to_csv(data_path, index=False)
+    print(f"{entity_name}_df is loaded")
     return df
-
-def get_dataframes():
-    users_path = 'saved_data/users_df.csv'
-    courses_path = 'saved_data/courses_df.csv'
-    classes_path = 'saved_data/classes_df.csv'
-    lessons_path = 'saved_data/lessons_df.csv'
-    lessons_records_path = 'saved_data/lessons_records_df.csv'
-    joins_path = 'saved_data/joins_df.csv'
-
-    users_df = make_or_read(users_path, api.get_users)
-    courses_df = make_or_read(courses_path, api.get_courses)
-    classes_df = make_or_read(classes_path, api.get_classes)
-    lessons_df = make_or_read(lessons_path, api.get_lessons)
-    lessons_records_df = make_or_read(lessons_records_path, api.get_lesson_records)
-    joins_df =  make_or_read(joins_path, api.get_joins)
-
-    return users_df, courses_df, classes_df, lessons_df, lessons_records_df, joins_df
-
 
 if __name__ == '__main__':
     API_KEY = credentials.API_KEY
 
     api = MoyClassAPI(api_key=API_KEY)
-    # print(f"Branches : {api.get_company_branches()}")
-    # print(f"Rooms : {api.get_company_rooms()}")
-    # print(f"Managers : {api.get_company_managers()}")
-    # print(f"Students : {api.get_students()}")
-    # print(f"Records : {api.get_records()}") # + record info
-    # print(f"Courses : {api.get_courses()}")
-    # print(f"Classes : {api.get_classes()}") # + class info
-    # print(f"Lessons : {api.get_lessons()}") # + lesson info
-    # print(f"Lesson records : {api.get_lesson_records()}") # + lesson_record info
 
-    # users_df, courses_df, classes_df, lessons_df, lessons_records_df, joins_df = get_dataframes()
+    # # Example 1: Get all managers:
+    # print(f"Managers as list : {api.get_company_managers()}")
+    # print(f"Managers as dataframe : {data_load(api.get_company_managers, 'managers')}")
+    #
+    # # Example 2: Get all lessons as dataframe:
+    # print(f"lessons as dataframe : {data_load(api.get_lessons, 'lessons')}")
 
-    # Get clients list
+    # Example 3: Get all users as dataframe:
+    load = False
+    user_df = data_load(api.get_users, 'users', load_new_data=load)
+    id_name_dict = pd.Series(user_df.name.values, index=user_df.id.values).to_dict()
+    id_status_dict = pd.Series(user_df.clientStateId.values, index=user_df.id.values).to_dict()
+    id = user_df[['name', 'clientStateId','id']].set_index('id').to_dict(orient = 'list')
+    print(id)
+    print('============')
+    print('============')
+    print('============')
+    # Get last month (last 31 days) lessons with lesson records:
+    from_date = datetime.today().date() - timedelta(days=31)
+    to_date = datetime.today().date()
+    params = [['date', f"{from_date}"], ['date', f"{to_date}"], ['includeRecords','true']]
+    lessond_with_records_df = data_load(api.get_lessons, 'lessons', params=params, load_new_data=load)
 
-    # pprint(api.get_users()['stats'])
+    drop_cols = ['beginTime', 'endTime', 'createdAt', 'filialId', 'roomId', 'comment', 'maxStudents',
+            'topic', 'description', 'teacherIds', 'status']
+    lessond_with_records_df = lessond_with_records_df.drop(drop_cols, axis = 1)
+    # user_visits structure:
+    # {userId: {classId: [['date', 'visit_status']]}}
+    user_visits = {}
+    for index, row in lessond_with_records_df.iterrows():
+        classId = row['classId']
+        date = row['date']
+        record = row['records']
+        if(len(record)):
+            if(type(record)==str):
+                record = record.replace("\'", "\"")
+                record = record.replace("True", "\"True\"")
+                record = record.replace("False", "\"False\"")
+                record = record.replace("None", "\"None\"")
+                data = json.loads(record)
+            else:
+                data = record
+            for el in data:
+                userId = el['userId']
+                lessonId = el['lessonId']
+                visit = 1 if el['visit']=='True' else 0
+                if(userId in user_visits):
+                    if(classId in user_visits[userId].keys()):
+                        user_visits[userId][classId].append([date, visit])
+                    else:
+                        user_visits[userId][classId] = [[date, visit]]
+                else:
+                    user_visits[userId] = {classId : [[date, visit]]}
 
 
-    uid = 1715582 #1741162
+    # user_visits[1] = {
+    #     1: [['2021-10-13', 0], ['2019-10-13', 0], ['2020-10-13', 0]],
+    #     2: [['2021-10-13', 1], ['2021-01-13', 0]],
+    #     3: [['2021-10-13', 1]]
+    # }
+    # user_status[1]=98582
 
-    u_info = api.get_user_info(uid)
-    print("USER INFO:")
-    pprint(u_info)
-    # Joins:
-    joins_classes = {join['id']:join['classId'] for join in u_info['joins']}
-    classes_joins = {join['classId']:join['id'] for join in u_info['joins']}
-    print(f"User {uid} joins_ids to classes_ids: {joins_classes}")
-    print(f"User {uid} classes_ids to joins_ids: {classes_joins}")
 
-    params = [['userId',f'{uid}'], ['date','2021-10-08'],['date', '2021-11-08']]
+    good_students = []
+    bad_students = []
+    for userId in user_visits.keys():
+        isBad = True
+        # Статус учится - 98582
+        if (id_status_dict[userId] != 98582):
+            continue
+        for classId in user_visits[userId]:
+            dates_visits = user_visits[userId][classId]
+            dates_visits.sort(key=lambda el : datetime.strptime(el[0], "%Y-%m-%d"))
+            if(len(dates_visits)==1):
+                if(dates_visits[-1][1] == 1):
+                    isBad = False
+            elif (len(dates_visits) > 1):
+                if (dates_visits[-1][1] == 1 or dates_visits[-2][1] == 1 ):
+                    isBad = False
+        user_visits[userId]['isBad'] = isBad
+        if(isBad == True):
+            bad_students.append(userId)
+        else:
+            good_students.append(userId)
+    # print(user_status_id)
+    # pprint(user_visits)
 
-    less = api.get_lessons(params)
-    l_df = resp_df(less)
-    print(f"LESSONS: ")
+    print(f"{len(bad_students)} BAD STUDENTS:")
+    for sId in bad_students:
+        print(f"{sId}: {id_name_dict[sId]}")
+
+
+
+
+# Joins:
+    # joins_classes = {join['id']:join['classId'] for join in u_info['joins']}
+    # classes_joins = {join['classId']:join['id'] for join in u_info['joins']}
+    # print(f"User {uid} joins_ids to classes_ids: {joins_classes}")
+    # print(f"User {uid} classes_ids to joins_ids: {classes_joins}"
+
+
+    # #Lesson records:
+    # Get last month (last 31 days) lesson records:
+    # from_date = datetime.today().date() - timedelta(days=31)
+    # to_date = datetime.today().date()
+    #
+    # params = [['date', f"{from_date}"], ['date', f"{to_date}"]]
+    # lr_df_path = 'saved_data/lessonRecords_df.csv'
+    # lr_df = last_month_data_load(api.get_lesson_records, 'lessonRecords', lr_df_path, params, False)
+    # print(f"lesson_records columns: {list(lr_df.columns)}")
+    #
+    # drop_cols = ['free', 'goodReason', 'test', 'paid', 'paid', 'createdAt', 'bill', 'userSubscription']
+    # lr_df = lr_df.drop(drop_cols, axis = 1)
+    # print(lr_df.head())
+
+
+
+"""
+    l_df_path = 'saved_data/lessons_df.csv'
+    l_df, lr_df = records_from_lessons(True)
+    print(f"lesson columns: {list(l_df.columns)}")
+
+    drop_cols = ['createdAt', 'filialId', 'roomId', 'classId', 'comment', 'maxStudents',
+                 'topic', 'description', 'teacherIds',]
+    l_df = l_df.drop(drop_cols, axis=1)
     print(l_df.head())
+    """
 
-    less = api.get_lesson_records(params)['lessonRecords']
-    lr_df = resp_df(less)
-    print(f"LESSONS RECORDS: ")
-    print(lr_df.head(30))
-
-
+    #
+    #
+    # uid = 1715582 #1741162
+    #
+    # u_info = api.get_user_info(uid)
+    # print("USER INFO:")
+    # pprint(u_info)
+    # # Joins:
+    # joins_classes = {join['id']:join['classId'] for join in u_info['joins']}
+    # classes_joins = {join['classId']:join['id'] for join in u_info['joins']}
+    # print(f"User {uid} joins_ids to classes_ids: {joins_classes}")
+    # print(f"User {uid} classes_ids to joins_ids: {classes_joins}")
+    #
+    #
+    #
+    # less = api.get_lessons(params)
+    # l_df = resp_df(less)
+    # print(f"LESSONS: ")
+    # print(l_df.head())
+    #
+    #
+    # lr_df = resp_df(less)
+    # print(f"LESSONS RECORDS: ")
+    # print(lr_df.head(30))
+    #
+    #
--- moyclass.py
+++ moyclass.py
@@ -39,7 +39,7 @@ class MoyClassAPI:
     ##################### G E T   M E T H O D S #####################
     #################################################################
 
-    def _get_request(self, url, get_auth="default", headers="default", params="default"):
+    def _get_request(self, url, get_auth="default", headers="default", params=None):
         """
         Get request template
         """
@@ -47,8 +47,6 @@ class MoyClassAPI:
             get_auth = {'apiKey':self.api_key}
         if(headers == "default"):
             headers = {"x-access-token": self.token}
-        if(params == "default"):
-            params = None
         r = requests.get(
             url=url,
             json=get_auth,
@@ -58,132 +56,132 @@ class MoyClassAPI:
         if (r.status_code == 200):
             return r.json()
         else:
-            print(f"Error: {r.status_code}")
+            print(f"Error: {r.status_code}, Message: {r.json()['code']}")
             return None
 
-    def get_company_branches(self):
+    def get_company_branches(self, params=None):
         """
         Returns a list of branches
         """
         url = "https://api.moyklass.com/v1/company/filials"
-        return self._get_request(url)
+        return self._get_request(url, params=params)
 
-    def get_company_rooms(self):
+    def get_company_rooms(self, params=None):
         """
         Company audiences
         """
         url = "https://api.moyklass.com/v1/company/rooms"
-        return self._get_request(url)
+        return self._get_request(url, params=params)
 
-    def get_company_managers(self):
+    def get_company_managers(self, params=None):
         """
         Company employees
         """
         url = "https://api.moyklass.com/v1/company/managers"
-        return self._get_request(url)
+        return self._get_request(url, params=params)
 
-    def get_users(self):
+    def get_users(self, params=None):
         """
         Company users (clients / students )
         """
         url = "https://api.moyklass.com/v1/company/users"
-        return self._get_request(url)
+        return self._get_request(url, params=params)
 
-    def get_user_info(self, uid):
+    def get_user_info(self, uid, params=None):
         """
         uid: user id
         Returns info about the user
         """
         url = f"https://api.moyklass.com/v1/company/users/{uid}"
-        return self._get_request(url)
+        return self._get_request(url, params=params)
 
-    def get_joins(self):
+    def get_joins(self, params=None):
         """
         Returns a list of joins ( requests / records ) in groups ( Список заявок )
         """
         url = "https://api.moyklass.com/v1/company/joins"
-        return self._get_request(url)['joins']
+        return self._get_request(url, params=params)
 
-    def get_joins_info(self, jid):
+    def get_joins_info(self, jid, params=None):
         """
         jid: join id
         Returns info about the join ( request / record ) ( Информация о заявке )
         """
         url = f"https://api.moyklass.com/v1/company/joins/{jid}"
-        return self._get_request(url)
+        return self._get_request(url, params=params)
 
-    def get_courses(self):
+    def get_courses(self, params=None):
         """
         Returns a list of courses ( Список программ )
         """
         url = "https://api.moyklass.com/v1/company/courses"
-        return self._get_request(url)
+        return self._get_request(url, params=params)
 
-    def get_classes(self):
+    def get_classes(self, params=None):
         """
         Returns a list of classes ( Список групп )
         """
         url = "https://api.moyklass.com/v1/company/classes"
-        return self._get_request(url)
+        return self._get_request(url, params=params)
 
-    def get_class_info(self, cid):
+    def get_class_info(self, cid, params=None):
         """
         cid: class id
         Returns info about the class ( Информация о группе )
         """
         url = f"https://api.moyklass.com/v1/company/classes/{cid}"
-        return self._get_request(url)
+        return self._get_request(url, params=params)
 
-    def get_lessons(self, params="default"):
+    def get_lessons(self, params=None):
         """
         Returns a list of lessons ( Список занятий )
         """
         url = "https://api.moyklass.com/v1/company/lessons"
-        return self._get_request(url, params=params)['lessons']
+        return self._get_request(url, params=params)
 
-    def get_lesson_info(self, lid):
+    def get_lesson_info(self, lid, params=None):
         """
         lid: lesson id
         Returns info about the lesson ( Информация о занятии )
         """
         url = f"https://api.moyklass.com/v1/company/lessons/{lid}"
-        return self._get_request(url)
+        return self._get_request(url, params=params)
 
-    def get_lesson_records(self, params="default"):
+    def get_lesson_records(self, params=None):
         """
         Returns a list of lessonRecords ( Список записей на занятия )
         """
         url = "https://api.moyklass.com/v1/company/lessonRecords"
-        return self._get_request(url, params=params)#['lessonRecords']
+        return self._get_request(url, params=params)
 
-    def get_lesson_record_info(self, lrid):
+    def get_lesson_record_info(self, lrid, params=None):
         """
         lrid: lesson record id
         Returns info about the lesson record ( Информация о записи на занятие )
         """
         url = f"https://api.moyklass.com/v1/company/lessonRecords/{lrid}"
-        return self._get_request(url)
+        return self._get_request(url, params=params)
 
-    def get_(self):
+    def get_(self, params=None):
         """
         Returns a list of
         """
         url = ""
-        return self._get_request(url)
+        return self._get_request(url, params=params)
 
-    def get__info(self, id):
+    def get__info(self, id, params=None):
         """
         id:  id
         Returns info about the  ( Информация о  )
         """
         url = f"/{id}"
-        return self._get_request(url)
+        return self._get_request(url, params=params)
 
     #################################################################
     #################### P O S T   M E T H O D S ####################
     #################################################################
 
-    def _post_request(self, url, post_auth="default", headers="default"):
+    def _post_request(self, url, post_auth="default", headers="default", params=None):
         """
         Post request template
         """
@@ -195,6 +193,7 @@ class MoyClassAPI:
             url = url,
             json = post_auth,
             headers = headers,
+            params = params
         )
         if(r.status_code == 200):
             print('Token granted')
